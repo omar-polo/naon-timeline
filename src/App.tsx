b3cfb197121167cfb9fd1301eb3bebbf3550cfb8
Omar Polo <op@omarpolo.com>
2026-06-02 14:33:48 +0000

    fix responsive & closing of left panel

--- src/App.tsx
+++ src/App.tsx
@@ -70,9 +70,9 @@ const Panel = ({mark, close} : {mark: IMarker, close(): void}) => {
       <div className={`absolute bottom-0 right-0 left-0 top-0 bg-gray-900/60 z-10000 ${!show && 'hidden'}`}
         onClick={_ => close()}>
       </div>
-      <div className={`absolute bottom-0 right-0 left-0 top-0 w-1/3 z-10100 bg-white p-4 transition-transform duration-300 ease-in-out ${show ? 'translate-0' : '-translate-x-full'}`}
+      <div className={`absolute bottom-0 right-0 left-0 top-0 w-full md:w-2/3 lg:w-1/3 z-10100 bg-white transition-transform duration-300 ease-in-out ${show ? 'translate-0' : '-translate-x-full'}`}
         onClick={_ => close()}>
-        <div onClick={e => e.stopPropagation()}>
+        <div onClick={e => e.stopPropagation()} className="w-full h-full p-4">
           { show && <PanelBody mark={mark} />}
         </div>
       </div>
